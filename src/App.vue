61b521d4b1b2f691fe4b0abf337c9a851a13a07f
bodaraharshil <developer.harshilbodara@gmail.com>
2021-03-17 23:58:42 +0530

    added setup for tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <div id="app">
-    <router-view>
-    </router-view>
+<figure class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+  <img class="w-32 h-32 rounded-full mx-auto" src="https://images.unsplash.com/photo-1615982822689-313e28327482?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" width="384" height="512">
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-semibold">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-cyan-600">
+        Sarah Dayan
+      </div>
+      <div class="text-gray-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
   </div>
 </template>
+
+
 
 <script>
 
@@ -12,13 +32,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/tailwind.css"/>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router.js';
 
-// Vuesax Component Framework
-import Vuesax from "vuesax";
-// import "material-icons/iconfont/material-icons.css"; //Material Icons
-import "vuesax/dist/vuesax.css"; // Vuesax
-Vue.use(Vuesax);
-
-// Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
